279d874ed4128e66be870d55b18138e989dcca44
q4w3h76 <q4w3h76@internet.ru>
2024-10-07 16:02:20 +0500

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'avatar_url' => fake()->unique()->imageUrl(),
+            'about' => fake()->unique()->text(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
