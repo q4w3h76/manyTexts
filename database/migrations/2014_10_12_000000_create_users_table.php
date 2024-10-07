d612cce24ee8147102fcb4894219637a8d9559c5
q4w3h76 <q4w3h76@internet.ru>
2024-10-07 14:44:13 +0500

    add new fields to users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar_url');
+            $table->string('about');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
