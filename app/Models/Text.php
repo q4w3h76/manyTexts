cba1aec9ea4d2e8f71c0e2e14bb2230220101703
q4w3h76 <q4w3h76@internet.ru>
2024-10-08 17:03:39 +0500

    add scope public method

--- app/Models/Text.php
+++ app/Models/Text.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
@@ -23,6 +24,10 @@ class Text extends Model
     protected $casts = [
         'is_public' => 'boolean'
     ];
+
+    public function scopePublic(Builder $query) {
+        return $query->whereIsPublic(true);
+    }
 
     protected static function boot()
     {
