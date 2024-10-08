3ca0d8b3fdd6310a3c717d3d2c2868823617b26f
q4w3h76 <q4w3h76@internet.ru>
2024-10-08 17:34:40 +0500

    add new routes for text resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TextController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::controller(TextController::class)->name('text.')->group(function () {
+    Route::get('', 'index')->name('index');
+    Route::post('', 'store')->name('store');
+    Route::get('{slug}', 'show')->name('show');
+    Route::put('{slug}', 'update')->name('update');
+    Route::delete('{slug}', 'destroy')->name('destroy');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
