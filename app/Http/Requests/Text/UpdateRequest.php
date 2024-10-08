6c39cdeea468db370cb0cd340d70f3314d0ad7a5
q4w3h76 <q4w3h76@internet.ru>
2024-10-08 17:05:40 +0500

    create requests for text

---
+++ app/Http/Requests/Text/UpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Text;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['string'],
+            'text' => ['string', 'min:1'],
+            'tags' => ['array'],
+            'is_public' => ['boolean'],
+            'expiration' => ['integer', 'min:0', 'max:527040'],
+        ];
+    }
+}
